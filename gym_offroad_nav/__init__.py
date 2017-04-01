27d4035ba36540c5b9a4c12676ae834e9c32a585
Po-Wei Chou <poweic@andrew.cmu.edu>
2017-04-01 11:26:22 -0400

    Change timestep_limit to infinity

--- gym_offroad_nav/__init__.py
+++ gym_offroad_nav/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 register(
     id='OffRoadNav-v0',
     entry_point='gym_offroad_nav.envs:OffRoadNavEnv',
-    timestep_limit=1000,
+    timestep_limit=100000000000,
 )

484745b10d751670f93fc82215cb346bc0a1f2db
Po-Wei Chou <poweic@andrew.cmu.edu>
2017-04-06 13:19:48 -0400

    Use cython to speed up rotated rect

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#
+# Enable cython support for eval scripts
+# Run as
+# setup.py build_ext --inplace
+#
+# WARNING: Only tested for Ubuntu 64bit OS.
+
+try:
+    from distutils.core import setup
+    from distutils.extension import Extension
+    from Cython.Build import cythonize
+except:
+    print "Unable to setup. Please use pip to install: cython"
+    print "sudo pip install cython"
+import os
+import numpy
+
+os.environ["CC"]  = "g++"
+os.environ["CXX"] = "g++"
+
+ext = Extension(
+    "gym_offroad_nav/rotated_rect/rotated_rect", ["gym_offroad_nav/rotated_rect/rotated_rect.pyx"],
+    include_dirs = [numpy.get_include(), '/usr/local/include/opencv', '/usr/local/include'],
+    libraries = ['opencv_core', 'opencv_highgui', 'opencv_imgproc'],
+    extra_compile_args=['-Wno-cpp', '-std=c++11'],
+    language="c++"
+)
+
+setup(ext_modules = cythonize(ext))
